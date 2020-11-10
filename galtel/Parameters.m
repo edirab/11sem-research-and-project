d34fbd503f8ea6b85565dfda614944a20aa9ea84
edirab <edirab@yandex.ru>
2020-11-10 15:31:48 +0300

    Доработан скрипт Parameters.m, получены вектора z_final, x_final и direction

--- galtel/Parameters.m
+++ galtel/Parameters.m
@@ -1,5 +1,6 @@
 % *************************************************
 % *** Параметры, необходимые для расчёта траектории 
+% *************************************************
 
 % Диаметры, ограничивающие перемещение центра масс, м
 % (см. чертёж)
@@ -12,7 +13,8 @@ angle = 90;
 
 % Вычислим количесво требуемых секторов
 N = fix(360 / angular_delta);
-display(class(N));
+
+%disp('class N:'); display(class(N));
 
 % Вектора с координатами
 z_outer = zeros(1, int32(N));
@@ -20,8 +22,50 @@ x_outer = zeros(1, int32(N));
 z_inner = zeros(1, int32(N));
 x_inner = zeros(1, int32(N));
 
+for i=1:N
+    %disp( class(i) ); disp( i );
+    
+    [p1_z, p1_x] = pol2cart(deg2rad(angle), D_mass_center_max);
+    [p2_z, p2_x] = pol2cart(deg2rad(angle + angular_delta/2), D_mass_center_min);
+    
+    %disp(p1_z); disp(p1_x);
+    
+    z_outer(int32(i)) = p1_z;
+    x_outer(int32(i)) = p1_x;
+    
+    z_inner(int32(i)) = p2_z;
+    x_inner(int32(i)) = p2_x;
+    
+    angle = angle + angular_delta;
+    
+end
+
+
+z_final = zeros(1, int32(N*2));
+x_final = zeros(1, int32(N*2));
+direction = zeros(1, int32(N*2));
+
+j = 1;
+for i=1:N
+   z_final(j) = z_outer(i);
+   x_final(j) = x_outer(i);
+   direction(j) = 1;
+   j = j + 1;
+   
+   z_final(j) = z_inner(i);
+   x_final(j) = x_inner(i);
+   direction(j) = -1;
+   j = j + 1;
+end
+
+clear p1_z p1_x p2_z p2_x i j z_inner x_inner z_outer x_outer;
+
+%disp(z_final);
+%disp(x_final);
+
 % *************************************************
 % *** Переменные подводного аппарата
+% *************************************************
 
 m = 24.7; %24.7
 % ???
