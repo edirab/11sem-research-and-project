fc4c80e3d12e25fa625a9d63dcf4897b42916819
edirab <edirab@yandex.ru>
2020-12-24 21:53:08 +0300

    Посчитан коэффициент k2 марша

--- matlab/Calculations.m
+++ matlab/Calculations.m
@@ -15,16 +15,42 @@ lambda55 = 117.08;
 P1 = 60 * 9.81; % = 588 H, упор одного движителя
 U1 = 24; % В, напряжение
 
+% Для ПФ одного винтомотора
 K_thruster = P1 / U1; % = 24.5 H/B;
 T_thruster = 0.3; % с
 
 l = 0.225;
 
+% ПФ ДРК по маршу
+K_propulsion_x = K_thruster * 4;
+T_propulsion_x = T_thruster;
+
+% ПФ ДРК по курсу
+K_propulsion_w = K_thruster * 4 * l;
+T_propulsion_w = T_thruster;
+
+V_linearization = 0; % 0.2; 
+w_linearization = 0;
+
+% Получим передаточную функцию канала маршевой скорости
+K_Vx = 1 / (2*Cvx1 * V_linearization + Cvx2); 
+T_Vx = (m + lambda11)/(2*Cvx1 * V_linearization + Cvx2);
+
+
+% Получим передаточную функцию канала поворота по курсу
+K_omega_y = 1 / (2 * Cwy1 * w_linearization + Cwy2);
+T_omega_w = (Jyy + lambda55)/(2 * Cwy1 * w_linearization + Cwy2);
+
+
+T_v = (2*T_propulsion_x*(m+lambda11)*0.707)/...
+    ( (2*Cvx1 * V_linearization + Cvx2)*T_propulsion_x + m + lambda11);
+
+k2_m = ( T_propulsion_x*(m+lambda11) - (2*Cvx1 * V_linearization + Cvx2)* T_v^2) /...
+    ( T_v^2 * K_propulsion_x );
+
 % Коэффициенты, которые необходимо синтезировать
 k1_m = 600; %4066;
-k2_m = 500; %527.58;
-
-
+%k2_m = 500; %527.58;
 
 % Коэффициенты, которые необходимо синтезировать
 %k1_k = 1;
