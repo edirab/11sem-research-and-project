753be2fec1f001d3f6f40281cda49b16d1fadbd4
Eugene Andreev <edirab@yandex.ru>
2021-01-16 15:48:17 +0300

    Все параметры аппарата в скрипте теперь разбиты на структуры. Небольшая реорганизация каталогов

--- MT-2010/Parameters_2.m
+++ MT-2010/Parameters_2.m
@@ -4,29 +4,31 @@
 
 % Диаметры, ограничивающие перемещение центра масс, м
 % (см. чертёж)
-D_mass_center_max = 13;
-D_mass_center_min = 5.8;
+Path = struct;
+
+Path.D_mass_center_max = 13;
+Path.D_mass_center_min = 5.8;
 
 % Угол сектора и полный угол, на который планируется повернуть 
-angular_delta = 15;
-angle = 90;
+Path.angular_delta = 15;
+Path.angle = 90;
 
 % Вычислим количесво требуемых секторов
-N = fix(360 / angular_delta);
+Path.N_points = fix(360 / Path.angular_delta);
 
 %disp('class N:'); display(class(N));
 
 % Вектора с координатами
-z_outer = zeros(1, int32(N));
-x_outer = zeros(1, int32(N));
-z_inner = zeros(1, int32(N));
-x_inner = zeros(1, int32(N));
+z_outer = zeros(1, int32(Path.N_points));
+x_outer = zeros(1, int32(Path.N_points));
+z_inner = zeros(1, int32(Path.N_points));
+x_inner = zeros(1, int32(Path.N_points));
 
-for i=1:N
+for i=1:Path.N_points
     %disp( class(i) ); disp( i );
     
-    [p1_z, p1_x] = pol2cart(deg2rad(angle), D_mass_center_max);
-    [p2_z, p2_x] = pol2cart(deg2rad(angle + angular_delta/2), D_mass_center_min);
+    [p1_z, p1_x] = pol2cart(deg2rad(Path.angle), Path.D_mass_center_max);
+    [p2_z, p2_x] = pol2cart(deg2rad(Path.angle + Path.angular_delta/2), Path.D_mass_center_min);
     
     %disp(p1_z); disp(p1_x);
     
@@ -36,25 +38,25 @@ for i=1:N
     z_inner(int32(i)) = p2_z;
     x_inner(int32(i)) = p2_x;
     
-    angle = angle + angular_delta;
+    Path.angle = Path.angle + Path.angular_delta;
     
 end
 
 % Инициализируем нулями массивы для точек итоговой траектории
-z_final = zeros(1, int32(N*2));
-x_final = zeros(1, int32(N*2));
-direction = zeros(1, int32(N*2));
+Path.z = zeros(1, int32(Path.N_points*2));
+Path.x = zeros(1, int32(Path.N_points*2));
+Path.direction = zeros(1, int32(Path.N_points*2));
 
 j = 1;
-for i=1:N
-   z_final(j) = z_outer(i);
-   x_final(j) = x_outer(i);
-   direction(j) = 1;
+for i=1:Path.N_points
+   Path.z(j) = z_outer(i);
+   Path.x(j) = x_outer(i);
+   Path.direction(j) = 1;
    j = j + 1;
    
-   z_final(j) = z_inner(i);
-   x_final(j) = x_inner(i);
-   direction(j) = -1;
+   Path.z(j) = z_inner(i);
+   Path.x(j) = x_inner(i);
+   Path.direction(j) = -1;
    j = j + 1;
 end
 
@@ -66,33 +68,35 @@ clear p1_z p1_x p2_z p2_x i j z_inner x_inner z_outer x_outer;
 % *************************************************
 % *** Переменные подводного аппарата
 % *************************************************
+AUV = struct;
+HydroDyn = struct;
 
-m = 300; %24.7
-Cvx1 = 182.87;
-Cvx2 = 33;
-lambda11 = 12.72;
+AUV.m = 300; %24.7
+HydroDyn.Cvx1 = 182.87;
+HydroDyn.Cvx2 = 33;
+AUV.lambda11 = 12.72;
 
-Jyy = 366;
-Cwy1 = 1144.7;
-Cwy2 = 22.52;
-lambda55 = 117.08;
+AUV.Jyy = 366;
+HydroDyn.Cwy1 = 1144.7;
+HydroDyn.Cwy2 = 22.52;
+AUV.lambda55 = 117.08;
 
-P1 = 60 * 9.81; % = 588 H, упор одного движителя
-U1 = 24; % В, напряжение
+AUV.P1 = 60 * 9.81; % = 588 H, упор одного движителя
+AUV.U1 = 24; % В, напряжение
 
 % Для ПФ одного винтомотора
-K_thruster = P1 / U1; % = 24.5 H/B;
-T_thruster = 0.3; % с
+AUV.K_thruster = AUV.P1 / AUV.U1; % = 24.5 H/B;
+AUV.T_thruster = 0.3; % с
 
-l = 0.225;
-
-% Коэффициенты, которые необходимо синтезировать
-k1_m = 4.5227;
-k2_m = 5.32;
+AUV.l = 0.225;
 
 % Коэффициенты, которые необходимо синтезировать
-k1_yaw = 0.5146;
-k2_yaw = 0.6370;
+AUV.k1_m = 4.5227;
+AUV.k2_m = 5.32;
+
+% Коэффициенты, которые необходимо синтезировать
+AUV.k1_yaw = 0.5146;
+AUV.k2_yaw = 0.6370;
 
 % коэффициент перевода из радиан в градусы
 kc = 57.2958;
@@ -103,10 +107,10 @@ Vfwd = Vfwd(1);
 
 %Радуис столкновения == описанной окружности по нижнему основанию
 % + половина длины АНПА
-Rcrash = sqrt(2) + 1.5;
+Path.Rcrash = sqrt(2) + 1.5;
 
 % Радиус точки траектории
-Rmin = 0.4;
+Path.Rmin = 0.4;
 
 %Течение направлено вдоль оси Z
 Vz = 0;
--- MT-2010/readme.md
+++ MT-2010/readme.md
@@ -10,5 +10,5 @@ set_param('like_a_star_1','SimulationCommand','Start');
 ### Конвертация timeseries в массив
 
 xlswrite('./timeseries/Time-X-Z.xlsx', [X_timeseries.Time X_timeseries.Data Z_timeseries.Data]);
-xlswrite('./timeseries/Time-X-Z-Yaw.xlsx', [X_timeseries.Time X_timeseries.Data Z_timeseries.Data Yaw_tiemseries.Data Yaw_tiemseries.Data]);
+xlswrite('./timeseries/Time-X-Z-Yaw.csv', [X_timeseries.Time X_timeseries.Data Z_timeseries.Data Yaw_tiemseries.Data]);
 
